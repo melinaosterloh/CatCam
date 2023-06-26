19ccce8a883c2eb04765954ebe73f5fd458d8c1a
melinaosterloh <83909493+melinaosterloh@users.noreply.github.com>
2023-06-26 12:54:03 +0200

    Add files via upload

---
+++ CatCam/CatCam/Classes/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MainViewController.swift
+//  CatCam
+//
+//  Created by Melina Osterloh on 20.06.23.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    
+
+}
